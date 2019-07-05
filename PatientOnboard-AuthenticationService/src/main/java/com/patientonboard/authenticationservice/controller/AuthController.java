cfbddb1367525aef2ef7b9ee69d0080cc536107f
JacobShanklin <Jacob.Shanklin.2@gmail.com>
2019-07-05 11:06:14 -0400

    removed unused imports

--- PatientOnboard-AuthenticationService/src/main/java/com/patientonboard/authenticationservice/controller/AuthController.java
+++ PatientOnboard-AuthenticationService/src/main/java/com/patientonboard/authenticationservice/controller/AuthController.java
@@ -3,11 +3,9 @@ package com.patientonboard.authenticationservice.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.patientonboard.authenticationservice.dao.UserDao;
