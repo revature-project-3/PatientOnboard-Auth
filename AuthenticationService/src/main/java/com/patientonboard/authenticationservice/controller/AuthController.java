9621c4b07368f1f1526b7f97cf912554fa0663f2
JacobShanklin <Jacob.Shanklin.2@gmail.com>
2019-07-09 09:54:59 -0400

    smell testing fixes

--- AuthenticationService/src/main/java/com/patientonboard/authenticationservice/controller/AuthController.java
+++ AuthenticationService/src/main/java/com/patientonboard/authenticationservice/controller/AuthController.java
@@ -5,8 +5,6 @@ import javax.persistence.PersistenceContext;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
